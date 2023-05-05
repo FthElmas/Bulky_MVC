d96899c59b6ff00233fc8f1faf5107f190d44862
FthElmas <fatih_elmas_34@hotmail.com>
2023-05-05 17:36:53 +0300

    SomeChanges

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyWeb.Data;
+using BulkyWeb.Models;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objectCategoryList = _db.Categories.ToList();
+            return View(objectCategoryList);
+        }
+    }
+}
