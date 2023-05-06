beb772eb56a40afa032c2fd272c7ea41e2c615d1
FthElmas <fatih_elmas_34@hotmail.com>
2023-05-07 00:16:42 +0300

    Category changes

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,32 @@ namespace BulkyWeb.Controllers
             List<Category> objectCategoryList = _db.Categories.ToList();
             return View(objectCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString() )
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if(ModelState.IsValid)
+            {
+               _db.Categories.Add(obj);
+               _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public IActionResult Delete(Category obj)
+        {
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
